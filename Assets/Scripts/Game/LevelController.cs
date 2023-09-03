2113c358e711aca16599c9e37e2cc8a689fe0c9a
Maxim Tsvetinsky <43120871+MakeZzzz@users.noreply.github.com>
2023-09-03 20:21:27 +0300

    Update LevelController.cs

--- Assets/Scripts/Game/LevelController.cs
+++ Assets/Scripts/Game/LevelController.cs
@@ -4,13 +4,7 @@ public class LevelController : MonoBehaviour
 {
     [SerializeField]  ObstacleSpawn _obstacleSpawn;
     [SerializeField] FireSpawn _fireSpawn;
-    // private InGameManager _inGameManager;
-    //
-    // public void Init(InGameManager value) 
-    // {
-    //     _inGameManager = value;
-    //     SetStartPosition();
-    // }
+
     public void SetStartPosition() 
     {
         _fireSpawn.DestroyFires();
